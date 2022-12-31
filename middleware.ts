fef704706d900f95e27a7e1bc24654163255d63e
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2022-12-31 11:14:12 -0500

    middleware updates

--- middleware.ts
+++ middleware.ts
@@ -4,12 +4,14 @@ import { withMiddlewareAuthRequired } from '@auth0/nextjs-auth0/edge'
 export default withMiddlewareAuthRequired(function middleware(
     request: NextRequest
 ) {
-    console.log(request.nextUrl.pathname)
-    if (request.nextUrl.pathname === '/api/users') {
-        return NextResponse.next()
-    }
+    return NextResponse.next()
 })
 
 export const config = {
-    matcher: '/api/users',
+    matcher: [
+        '/api/users',
+        '/api/users/documents/delete',
+        '/api/users/documents/new',
+        '/api/users/documents/save',
+    ],
 }
